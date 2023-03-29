b72ec364a6f607867a6026a03b8c5f546e231c17
Anna Volfman <aniya_07@ukr.net>
2023-03-29 22:58:59 +0300

    add commander and update readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# CLI-1
-Node.js
+# Screenshots
+#1
+https://drive.google.com/file/d/1Qd044OVAmj9E9nT-SuLq10TXJaEehmve/view?usp=sharing
+
+#2
+https://drive.google.com/file/d/1KZeQ1ZHVmENnx2vXmlV3qKCQIA43jT77/view?usp=sharing
+
+#3
+https://drive.google.com/file/d/15YO7HLTnNr8-vvnokL9zHi0u4QpkMaWO/view?usp=sharing
+
+#4
+https://drive.google.com/file/d/103iezpmFRvULUpUEWfmKLB0FuCsll1WH/view?usp=sharing
--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+const { Command } = require("commander");
+const program = new Command();
 const contacts = require("./contacts");
 
-contacts.listContacts();
-// contacts.getContactById('vza2RIzNGIwutCVCs4mCL');
-// contacts.removeContact("AeHIrLTr6JkxGE6SN-0Rw");
-// contacts.addContact('1', '2', '3')
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts.listContacts();
+      break;
+
+    case "get":
+      contacts.getContactById(id);
+      break;
+
+    case "add":
+      contacts.addContact(name, email, phone);
+      break;
+
+    case "remove":
+      contacts.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
